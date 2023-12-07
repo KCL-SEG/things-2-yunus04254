de47a59a147c5a8edb88be6afa2ed2507419b46a
yunus04254 <yunus.sufian1@gmail.com>
2023-12-07 14:01:13 +0000

    Complete exercise

--- things/models.py
+++ things/models.py
@@ -4,7 +4,5 @@ from django.db import models
 class Thing(models.Model):
     name = models.CharField(max_length=35, unique=True)
     description = models.CharField(max_length=120, blank=True)
-    quantity = models.IntegerField(
-        validators=[MinValueValidator(0),MaxValueValidator(50)]
-    )
+    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(50)])
     created_at = models.DateTimeField(auto_now_add=True)
--- things/views.py
+++ things/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .forms import ThingForm
 
 def home(request):
+    form = ThingForm()
+    if form.is_valid():
+        form.save()
     return render(request, 'home.html', {'form': form})
